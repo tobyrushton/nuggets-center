3da8c5d1b96b83bf65ad23a0b7c6d948065fd12b
tobyrushton <tobyjrushton@icloud.com>
2024-01-20 16:41:39 +0000

    added getDateOfGame function

---
+++ __tests__/lib/getDateOfGame.test.ts
@@ -0,0 +1,44 @@
+import { getDateOfGame } from '../../src/lib/getDateOfGame'
+
+describe('getDateOfGame()', () => {
+    it('should return a date object', () => {
+        const date = getDateOfGame('10/10')
+        expect(date).toBeInstanceOf(Date)
+    })
+
+    it('should return a date object with the correct date when correct year', () => {
+        jest.useFakeTimers().setSystemTime(new Date('2023-10-01'))
+        const date = getDateOfGame('10/10')
+        expect(date.getFullYear()).toBe(2023)
+        const date2 = getDateOfGame('10/25')
+        expect(date2.getFullYear()).toBe(2023)
+        const date3 = getDateOfGame('11/12')
+        expect(date3.getFullYear()).toBe(2023)
+        const date4 = getDateOfGame('12/12')
+        expect(date4.getFullYear()).toBe(2023)
+    })
+
+    it('should return a date object with the correct date when year ahead', () => {
+        jest.useFakeTimers().setSystemTime(new Date('2024-01-01'))
+        const date = getDateOfGame('10/10')
+        expect(date.getFullYear()).toBe(2023)
+        const date2 = getDateOfGame('10/25')
+        expect(date2.getFullYear()).toBe(2023)
+        const date3 = getDateOfGame('11/12')
+        expect(date3.getFullYear()).toBe(2023)
+        const date4 = getDateOfGame('12/12')
+        expect(date4.getFullYear()).toBe(2023)
+    })
+
+    it('should return a date object with the correct date when year behind', () => {
+        jest.useFakeTimers().setSystemTime(new Date('2023-10-01'))
+        const date = getDateOfGame('01/01')
+        expect(date.getFullYear()).toBe(2024)
+        const date2 = getDateOfGame('01/25')
+        expect(date2.getFullYear()).toBe(2024)
+        const date3 = getDateOfGame('02/12')
+        expect(date3.getFullYear()).toBe(2024)
+        const date4 = getDateOfGame('03/12')
+        expect(date4.getFullYear()).toBe(2024)
+    })
+})
---
+++ src/lib/getDateOfGame.ts
@@ -0,0 +1,28 @@
+/**
+ * @description returns the date of the game
+ * @param {string} date - the date of the game in format mm/dd
+ */
+export const getDateOfGame = (date:string):Date => {
+    const [month] = date.split('/')
+    const monthInt = parseInt(month)
+
+    const todaysYear = new Date().getFullYear()
+    const todaysMonth = new Date().getMonth()
+
+    let year: number = 0
+
+    if(todaysMonth > 6){
+        if(monthInt > 6)
+            year = todaysYear
+        else 
+            year = todaysYear + 1
+    }
+    else{
+        if(monthInt > 6)
+            year = todaysYear - 1
+        else 
+            year = todaysYear
+    }
+
+    return new Date(`${date} ${year}`)
+}
