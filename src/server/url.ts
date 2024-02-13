5db8942c469f5060b8eed2ae5d18597acdd40f82
tobyrushton <tobyjrushton@icloud.com>
2024-02-13 16:40:38 +0000

    updated prod url

--- src/server/url.ts
+++ src/server/url.ts
@@ -1,4 +1,4 @@
 export const url =
     process.env.NODE_ENV === 'production'
-        ? 'https://visionary-manatee-0747fe.netlify.app/api/trpc'
+        ? 'https://nuggets-center.vercel.app/api/trpc'
         : 'http://localhost:3000/api/trpc'
