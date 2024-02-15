a4d2fa07c945e2c551a45c73ef5cf4bb11a201c6
tobyrushton <tobyjrushton@icloud.com>
2024-02-15 17:53:38 +0000

    added team images to last five games

--- src/components/LastFiveGames.tsx
+++ src/components/LastFiveGames.tsx
@@ -2,6 +2,7 @@ import 'server-only'
 import { FC } from 'react'
 import { serverClient } from '@/app/_trpc/serverClient'
 import Link from 'next/link'
+import Image from 'next/image'
 import { Card, CardHeader, CardTitle, CardContent } from './ui/card'
 import { Separator } from './ui/separator'
 import { Skeleton } from './ui/skeleton'
@@ -19,11 +20,17 @@ export const LastFiveGamesSkeleton: FC = () => {
                             <div className="p-1 flex flex-row h-fit">
                                 <div className="flex grow flex-col px-2 gap-2">
                                     <span className="flex flex-row justify-between">
-                                        <Skeleton className="h-5 w-32" />
+                                        <span className="flex flex-row gap-2">
+                                            <Skeleton className="rounded-full h-6 w-6" />
+                                            <Skeleton className="h-5 w-32" />
+                                        </span>
                                         <Skeleton className="h-5 w-10" />
                                     </span>
                                     <span className="flex flex-row justify-between">
-                                        <Skeleton className="h-5 w-32" />
+                                        <span className="flex flex-row gap-2">
+                                            <Skeleton className="rounded-full h-6 w-6" />
+                                            <Skeleton className="h-5 w-32" />
+                                        </span>
                                         <Skeleton className="h-5 w-10" />
                                     </span>
                                 </div>
@@ -47,10 +54,13 @@ export const LastFiveGamesSkeleton: FC = () => {
 }
 
 export const LastFiveGames: FC = async () => {
-    const { schedule: lastFiveGames } = await serverClient.getSchedule({
-        take: 5,
-        method: 'last',
-    })
+    const [{ schedule: lastFiveGames }, { team }] = await Promise.all([
+        await serverClient.getSchedule({
+            take: 5,
+            method: 'last',
+        }),
+        await serverClient.getTeam({ name: 'Denver Nuggets' }),
+    ])
 
     return (
         <Card>
@@ -65,19 +75,53 @@ export const LastFiveGames: FC = async () => {
                                 <div className="p-1 flex flex-row h-fit">
                                     <div className="flex grow flex-col px-2">
                                         <span className="flex flex-row justify-between">
-                                            <p>
-                                                {game.home
-                                                    ? 'Denver Nuggets'
-                                                    : game.opponent.name}
-                                            </p>
+                                            <span className="flex flex-row gap-2">
+                                                <Image
+                                                    src={
+                                                        game.home
+                                                            ? team.logo_url
+                                                            : game.opponent
+                                                                  .logo_url
+                                                    }
+                                                    alt={
+                                                        game.home
+                                                            ? team.name
+                                                            : game.opponent.name
+                                                    }
+                                                    height={25}
+                                                    width={25}
+                                                />
+                                                <p>
+                                                    {game.home
+                                                        ? 'Denver Nuggets'
+                                                        : game.opponent.name}
+                                                </p>
+                                            </span>
                                             <p>{game.home_score}</p>
                                         </span>
                                         <span className="flex flex-row justify-between">
-                                            <p>
-                                                {game.home
-                                                    ? game.opponent.name
-                                                    : 'Denver Nuggets'}
-                                            </p>
+                                            <span className="flex flex-row gap-2">
+                                                <Image
+                                                    src={
+                                                        game.home
+                                                            ? game.opponent
+                                                                  .logo_url
+                                                            : team.logo_url
+                                                    }
+                                                    alt={
+                                                        game.home
+                                                            ? game.opponent.name
+                                                            : team.name
+                                                    }
+                                                    height={25}
+                                                    width={25}
+                                                />
+                                                <p>
+                                                    {game.home
+                                                        ? game.opponent.name
+                                                        : 'Denver Nuggets'}
+                                                </p>
+                                            </span>
                                             <p>{game.opponent_score}</p>
                                         </span>
                                     </div>
