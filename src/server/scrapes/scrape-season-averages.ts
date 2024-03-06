216da2a8028ddcd7d19af94ae51769d77d10233b
tobyrushton <tobyjrushton@icloud.com>
2024-03-06 20:52:17 +0000

    lint

--- src/server/scrapes/scrape-season-averages.ts
+++ src/server/scrapes/scrape-season-averages.ts
@@ -38,9 +38,8 @@ export const scrapeSeasonAverages = async (): Promise<
     await page.setViewport({
         width: 1920,
         height: 1080,
-      });
+    })
     await page.goto('https://www.espn.co.uk/nba/team/stats/_/name/den')
-
 
     const seasonAverages = await page.evaluate(() => {
         const tables = window.document.querySelectorAll('.Table')
