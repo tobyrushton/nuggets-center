3c5ac381e0eabaf06a1b864fd44a71e51f78aa9f
tobyrushton <tobyjrushton@icloud.com>
2024-04-18 19:30:14 +0100

    fix type error

--- src/server/functions/update-schedule.ts
+++ src/server/functions/update-schedule.ts
@@ -65,7 +65,7 @@ export const updateSchedule = async (): Promise<void> => {
                         home: game.home,
                         home_score: game.home_score,
                         opponent_score: game.opponent_score,
-                        type: game.gameType,
+                        type: game.type,
                     },
                 })
             })
